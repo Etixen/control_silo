1b3d6b13ad18f4ad424c232c2530bf084ca04353
Etixen <94640738+Etixen@users.noreply.github.com>
2022-08-31 07:21:50 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,58 @@
+let silos = [
+  {
+    name: "Silo 1",
+    lat: -34.59482,
+    lng: -58.69083,
+    segurity: "Guardia 1",
+    time: "10:53",
+    state: "Abierto"
+  },
+  {
+    name: "Silo 2",
+    lat: -34.59351,
+    lng: -58.68830,
+    segurity: "Guardia 2",
+    time: "07:24",
+    state: "Cerrado",
+  },
+  {
+    name: "Silo 3",
+    lat: -34.59266,
+    lng: -58.68577,
+    segurity: "Guardia 3",
+    time: "04:30",
+    state: "Abierto"
+  },
+  {
+    name: "Silo 4",
+    lat: -34.59402,
+    lng: -58.68165,
+    segurity: "Guardia 1",
+    time: "22:30",
+    state: "Cerrado",
+  },
+  {
+    name: "Silo 5",
+    lat: -34.59618,
+    lng: -58.68873,
+    segurity: "Guardia 4",
+    time: "23:59",
+    state: "Abierto",
+  },
+  {
+    name: "Silo 6",
+    lat: -34.59722,
+    lng: -58.68337,
+    segurity: "Guardia 5",
+    time: "00:30",
+    state: "Cerrado"
+  },
+
+  /* {
+    name: "Le Meridien - Ra",
+    lat: 41.1865574,
+    lng: 1.5628249,
+    address: "C/De la mar blava",
+    phone: "93 888888"
+  }, */
+];
